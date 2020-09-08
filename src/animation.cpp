2c0a4f88616b8286c38ae60df88b5e363dd079fa
My Name <foo@lisp.us.to>
2020-09-07 22:32:02 -0400

    Proper binary search in findKeyframe

--- src/animation.cpp
+++ src/animation.cpp
@@ -7,26 +7,29 @@ using namespace grendx;
 size_t animation::findKeyframe(float delta) {
 	assert(frametimes.size() != 0);
 	assert(delta >= 0);
-	size_t idx = 0;
 
-	for (size_t div = frametimes.size()/2; div; div /= 2) {
-		if (delta <= frametimes[div]
-			&& (idx == 0 || delta > frametimes[idx-1]))
+	size_t lower = 0;
+	size_t upper = frametimes.size() - 1;
+	size_t mid = (upper + lower)/2;
+
+	while (lower != upper) {
+		if (delta <= frametimes[mid]
+			&& (mid == 0 || delta > frametimes[mid-1]))
 		{
-			return idx;
+			return mid;
 		}
 
-		// TODO: need to set frametimes[0] to 0 when loading,
-		//       just to make sure it's 0
-		if (delta < frametimes[idx]) {
-			assert(idx != 0);
-			idx -= div;
+		if (delta < frametimes[mid]) {
+			upper = mid - 1;
+			mid = (upper + lower)/2;
+
 		} else {
-			idx += div;
+			lower = mid + 1;
+			mid = (upper + lower)/2;
 		}
 	}
 
-	return idx;
+	return mid;
 }
 
 std::tuple<size_t, size_t, float> animation::interpFrames(float delta) {
@@ -39,31 +42,17 @@ std::tuple<size_t, size_t, float> animation::interpFrames(float delta) {
 	size_t kdx = (idx + 1) % frametimes.size();
 
 	assert(idx < frametimes.size());
-	/*
-	if (idx == frametimes.size() - 1) {
-		puts("asdf");
-	}
-	*/
-	// XXX: TODO: proper time
-	//float range = fabs(frametimes[kdx] - frametimes[idx]);
-	//float interp = (adj - frametimes[idx])/range;
 	float interp = 0.0;
-
-	//assert(kdx);
 
 	if (kdx) {
 		float range = frametimes[kdx] - frametimes[idx];
 		interp = (adj - frametimes[idx])/range;
 
 	} else if (frametimes[0] > 0) {
-		//float range = frametimes[kdx] - frametimes[idx];
-		//interp = (adj - frametimes[idx])/range;
-		//interp = 0.0;
-		//float range = frametimes[0];
 		interp = adj / frametimes[0];
 
 	} else {
-		interp = 0.0;
+		interp = 1.0;
 	}
 
 	return {idx, kdx, interp};
