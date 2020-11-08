28c7a130925d5662fbdfae0dbaac95e200fd46bd
My Name <foo@lisp.us.to>
2020-11-08 08:38:44 -0500

    Third-person view, adding test guy

--- thing/main.cpp
+++ thing/main.cpp
@@ -13,20 +13,54 @@
 
 using namespace grendx;
 
+// TODO: only returns one collection, object tree could have any number
+animationCollection::ptr findAnimations(gameObject::ptr obj) {
+	if (!obj) {
+		return nullptr;
+	}
+
+	for (auto& chan : obj->animations) {
+		return chan->group->collection;
+	}
+
+	for (auto& [name, ptr] : obj->nodes) {
+		auto ret = findAnimations(ptr);
+
+		if (ret) {
+			return ret;
+		}
+	}
+
+	return nullptr;
+}
+
 // TODO: library functions to handle most of this
 void addCameraWeapon(gameView::ptr view) {
 	playerView::ptr player = std::dynamic_pointer_cast<playerView>(view);
 	gameObject::ptr testweapon = std::make_shared<gameObject>();
 
 	auto [objs, models] =
-		load_gltf_scene(GR_PREFIX "assets/obj/Mossberg-lowres/shotgun.gltf");
+		load_gltf_scene(GR_PREFIX "assets/obj/TestGuy/rigged-lowpolyguy.glb");
+		//load_gltf_scene(GR_PREFIX "assets/obj/Mossberg-lowres/shotgun.gltf");
+
+	// leaving old stuff here for debugging
+	objs->transform.scale = glm::vec3(0.1f);
+	//objs->transform.scale = glm::vec3(2.f);
+	//objs->transform.position = glm::vec3(-0.3, 1.1, 1.25);
+	//objs->transform.rotation = glm::quat(glm::vec3(0, 3.f*M_PI/2.f, 0));
 
 	compile_models(models);
 	bind_cooked_meshes();
-	objs->transform.scale = glm::vec3(2.0f);
-	objs->transform.position = glm::vec3(-0.3, 1.1, 1.25);
-	objs->transform.rotation = glm::quat(glm::vec3(0, 3.f*M_PI/2.f, 0));
 	setNode("weapon", player->cameraObj, objs);
+
+	// TODO: need better way to do this
+	if (auto animations = findAnimations(objs)) {
+		for (auto& [name, ptr] : animations->groups) {
+			if (auto group = ptr.lock()) {
+				group->weight = (name == "idle")? 1.0 : 0.0;
+			}
+		}
+	}
 }
 
 int main(int argc, char *argv[]) {
