3d7c31aedccb5dc44dab7279b48122fc1cf71ba4
My Name <foo@lisp.us.to>
2020-11-30 19:07:39 -0500

    Updating landscape demo with new stuff, async map generation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <grend/gameObject.hpp>
 #include <grend/playerView.hpp>
 #include <grend/geometryGeneration.hpp>
+#include <grend/gameEditor.hpp>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +13,10 @@
 #include <iostream>
 #include <memory>
 #include <chrono>
+
+// XXX:  toggle using textures/models I have locally, don't want to
+//       bloat the assets folder again
+#define LOCAL_BUILD 0
 
 using namespace grendx;
 
@@ -113,36 +118,57 @@ static float landscapeThing(float x, float y) {
 		return scale*perlinNoise(x / scale, y / scale);
 	};
 
-	float a1 = max(0.f, scalednoise(50.f, x, y));
-	float a2 = scalednoise(10.f, x, y);
-	float a3 = scalednoise(3.f,  x, y);
-	float a4 = scalednoise(0.5f, x, y);
+	float a1 = max(0.f, scalednoise(75.f, x, y));
+	float a2 = scalednoise(20.f, x, y);
+	float a3 = scalednoise(5.f,  x, y);
+	float a4 = 0.5*scalednoise(1.f, x, y);
 
 	return a1 + a2 + a3 + a4;
 }
 
 material::ptr landscapeMaterial;
 static gameObject::ptr landscapeNodes = std::make_shared<gameObject>();
+static gameModel::ptr treeNode;
 
-class landscapeGenView : public playerView {
+class worldGenerator {
 	public:
-		landscapeGenView(gameMain *game) : playerView(game) {};
-		virtual void logic(gameMain *game, float delta);
+		virtual gameObject::ptr getNode(void) { return root; };
+		virtual void setPosition(gameMain *game, glm::vec3 position) = 0;
+
+	protected:
+		gameObject::ptr root = std::make_shared<gameObject>();
+		glm::vec3 lastPosition = glm::vec3(INT_MAX);
+};
+
+class landscapeGenerator : public worldGenerator {
+	public:
+		landscapeGenerator(unsigned seed = 0xcafebabe);
+		virtual void setPosition(gameMain *game, glm::vec3 position);
+
+	private:
+		void generateLandscape(gameMain *game, glm::vec3 curpos, glm::vec3 lastpos);
 		std::future<bool> genjob;
+		gameObject::ptr returnValue;
 };
 
 static const int   gridsize = 9;
 static const float cellsize = 24.f;
 
-static gameObject::ptr
-generateLandscape(glm::vec3 curpos, glm::vec3 genpos, gameMain *game) {
+landscapeGenerator::landscapeGenerator(unsigned seed) {
+	// TODO: do something with the seed
+}
+
+void landscapeGenerator::generateLandscape(gameMain *game,
+                                           glm::vec3 curpos,
+                                           glm::vec3 lastpos)
+{
 	static gameModel::ptr models[gridsize][gridsize];
 	static gameModel::ptr temp[gridsize][gridsize];
 
 	gameObject::ptr ret = std::make_shared<gameObject>();
 	std::list<std::future<bool>> futures;
 
-	glm::vec3 diff = curpos - genpos;
+	glm::vec3 diff = curpos - lastpos;
 	std::cerr << "curpos != genpos, diff: "
 		<< "(" << diff.z << "," << diff.y << "," << diff.z << ")"
 		<< std::endl;
@@ -165,7 +191,7 @@ generateLandscape(glm::vec3 curpos, glm::vec3 genpos, gameMain *game) {
 					gameMesh::ptr mesh =
 						std::dynamic_pointer_cast<gameMesh>(ptr->nodes["mesh"]);
 					mesh->meshMaterial = landscapeMaterial;
-					std::string name = "gen["+std::to_string(int(x))+"]["+std::to_string(int(y))+"]" + std::to_string(rand());
+					std::string name = "gen["+std::to_string(int(x))+"]["+std::to_string(int(y))+"]";
 
 					gameObject::ptr foo = std::make_shared<gameObject>();
 					setNode("asdfasdf", foo, ptr);
@@ -178,7 +204,28 @@ generateLandscape(glm::vec3 curpos, glm::vec3 genpos, gameMain *game) {
 						return true;
 					});
 
+					glm::vec2 posgrad = randomGradient(glm::vec2(coord.x, coord.z));
+					float baseElevation = landscapeThing(coord.x, coord.z);
+					int randtrees = (posgrad.x + 1.0)*0.5 * 5 * (1.0 - baseElevation/50.0);
+
 					game->phys->addStaticModels(foo, TRS());
+
+					for (int i = 0; i < randtrees; i++) {
+						gameObject::ptr ntree = std::make_shared<gameObject>();
+						glm::vec2 pos = randomGradient(glm::vec2(coord.x + i, coord.z + i));
+
+						float tx = ((pos.x + 1)*0.5) * cellsize;
+						float ty = ((pos.y + 1)*0.5) * cellsize;
+
+						ntree->transform.position = glm::vec3(
+							tx, landscapeThing(coord.x + tx, coord.z + ty) - 0.1, ty
+						);
+						ntree->transform.scale = glm::vec3((posgrad.y + 1.0)*0.5*3.0+0.5);
+
+						std::string name = "tree["+std::to_string(i)+"]";
+						setNode("tree", ntree, treeNode);
+						setNode(name, ptr, ntree);
+					}
 					temp[x][y] = ptr;
 					fut.wait();
 					return true;
@@ -214,44 +261,96 @@ generateLandscape(glm::vec3 curpos, glm::vec3 genpos, gameMain *game) {
 	});
 
 	fut.wait();
-	return ret;
+	returnValue = ret;
 }
 
-static glm::vec3 genpos = glm::vec3(INT_MAX);
+void landscapeGenerator::setPosition(gameMain *game, glm::vec3 position) {
+	glm::vec3 curpos = glm::floor((glm::vec3(1, 0, 1)*position)/cellsize);
+
+	if (genjob.valid() && genjob.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
+		genjob.get();
+		setNode("nodes", root, returnValue);
+		returnValue = nullptr;
+	}
+
+	if (!genjob.valid() && curpos != lastPosition) {
+		std::cerr << "BBBBBBBBBB: starting new generator job" << std::endl;
+		glm::vec3 npos = lastPosition;
+		lastPosition = curpos;
+
+		genjob = game->jobs->addAsync([=] {
+			generateLandscape(game, curpos, npos);
+			return true;
+		});
+	}
+}
+
+class landscapeGenView : public playerView {
+	public:
+		typedef std::shared_ptr<landscapeGenView> ptr;
+		typedef std::weak_ptr<landscapeGenView>   weakptr;
+
+		landscapeGenView(gameMain *game) : playerView(game) {
+#if LOCAL_BUILD
+			enemyModel = loadScene("test-assets/obj/test-enemy.glb");
+#else
+			gameModel::ptr mod = generate_cuboid(1.0, 2.0, 0.5);
+			enemyModel = std::make_shared<gameObject>();
+			compileModel("emeny", mod);
+			setNode("model", enemyModel, mod);
+#endif
+
+			for (unsigned i = 0; i < 15; i++) {
+				gameObject::ptr temp = std::make_shared<gameObject>();
+
+				temp->transform.position = glm::vec3(
+					float(rand()) / RAND_MAX * 100.0 - 50,
+					50.0,
+					float(rand()) / RAND_MAX * 100.0 - 50
+				);
+
+				std::string name = "enemy["+std::to_string(i)+"]";
+				game->phys->addSphere(temp, temp->transform.position, 1.0, 1.0);
+				temp->physObj->setAngularFactor(0.0);
+				enemies.push_back(temp);
+
+				setNode("model", temp, enemyModel);
+				setNode(name, enemyNodes, temp);
+			}
+		};
+
+		virtual void logic(gameMain *game, float delta);
+		gameObject::ptr getEnemies(void) { return enemyNodes; };
+
+		std::vector<gameObject::ptr> enemies;
+		gameObject::ptr enemyNodes = std::make_shared<gameObject>();
+		gameObject::ptr enemyModel;
+		landscapeGenerator landscape;
+};
+
 void landscapeGenView::logic(gameMain *game, float delta) {
 	static glm::vec3 lastvel = glm::vec3(0);
 	static gameObject::ptr retval;
 
 	if (cam->velocity() != lastvel) {
-		cameraPhys->setAcceleration(cam->velocity());
+		cameraPhys->setVelocity(cam->velocity());
 		lastvel = cam->velocity();
 	}
 
 	game->phys->stepSimulation(1.f/game->frame_timer.last());
 	cam->setPosition(cameraObj->transform.position - 5.f*cam->direction());
 
+	for (auto& it : enemies) {
+		glm::vec3 diff = cameraObj->transform.position - it->transform.position;
+		glm::vec3 vel =  glm::normalize(glm::vec3(diff.x, 0, diff.z));
+		it->physObj->setAcceleration(10.f*vel);
+	}
+
 	glm::vec3 vel = cameraPhys->getVelocity();
 	cameraObj->transform.rotation =
 		glm::quat(glm::vec3(0, atan2(vel.x, vel.z), 0));
 
-	glm::vec3 curpos = glm::floor((glm::vec3(1, 0, 1)*cameraObj->transform.position)/25.f);
-
-	if (genjob.valid() && genjob.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready) {
-		genjob.get();
-		setNode("nodes", landscapeNodes, retval);
-		retval = nullptr;
-	}
-
-	if (!genjob.valid() && curpos != genpos) {
-		std::cerr << "BBBBBBBBBB: starting new generator job" << std::endl;
-		glm::vec3 npos = genpos;
-		genpos = curpos;
-
-		genjob = game->jobs->addAsync([=] {
-			retval = generateLandscape(curpos, npos, game);
-			return true;
-		});
-	}
+	landscape.setPosition(game, cameraObj->transform.position);
 }
 
 int main(int argc, char *argv[]) {
@@ -259,6 +358,13 @@ int main(int argc, char *argv[]) {
 	std::cerr << "started SDL context" << std::endl;
 	std::cerr << "have game state" << std::endl;
 
+	TRS staticPosition; // default
+	gameMain *game = new gameMainDevWindow();
+
+// XXX:  toggle using textures I have locally, don't want to bloat the assets
+//       folder again
+// TODO: include some default textures in assets for this, or procedurally generate
+#if LOCAL_BUILD
 	landscapeMaterial = std::make_shared<material>();
 	landscapeMaterial->factors.diffuse = {1, 1, 1, 1};
 	landscapeMaterial->factors.ambient = {1, 1, 1, 1};
@@ -280,9 +386,23 @@ int main(int argc, char *argv[]) {
 	landscapeMaterial->maps.emissive
 		= std::make_shared<materialTexture>(GR_PREFIX "assets/tex/black.png");
 
-	TRS staticPosition; // default
+	treeNode = load_object("assets-old/obj/Modular Terrain Hilly/Prop_Tree_Pine_3.obj");
 
-	gameMain *game = new gameMainDevWindow();
+#else
+	landscapeMaterial = std::make_shared<material>();
+	landscapeMaterial->factors.diffuse = {0.15, 0.3, 0.1, 1};
+	landscapeMaterial->factors.ambient = {1, 1, 1, 1};
+	landscapeMaterial->factors.specular = {1, 1, 1, 1};
+	landscapeMaterial->factors.emissive = {0, 0, 0, 0};
+	landscapeMaterial->factors.roughness = 0.9f;
+	landscapeMaterial->factors.metalness = 0.f;
+	landscapeMaterial->factors.opacity = 1;
+	landscapeMaterial->factors.refract_idx = 1.5;
+
+	treeNode = generate_cuboid(1.0, 1.0, 1.0);
+#endif
+
+	compileModel("treedude", treeNode);
 
 	game->jobs->addAsync([=] {
 		auto foo = openSpatialLoop(GR_PREFIX "assets/sfx/Bit Bit Loop.ogg");
@@ -299,30 +419,14 @@ int main(int argc, char *argv[]) {
 	});
 
 	game->state->rootnode = loadMap(game);
-	game->jobs->addAsync([=] {
-		static gameObject::ptr retval;
-		setNode("landscape", game->state->rootnode, landscapeNodes);
-		genpos = glm::vec3(0);
-
-		auto fut = game->jobs->addAsync([=] {
-			retval = generateLandscape(glm::vec3(0), glm::vec3(INT_MAX), game);
-			return true;
-		});
-
-		fut.wait();
-		auto bindfut = game->jobs->addDeferred([=] {
-			setNode("nodes", landscapeNodes, retval);
-			return true;
-		});
-
-		bindfut.wait();
-		return true;
-	});
-
 	game->phys->addStaticModels(game->state->rootnode, staticPosition);
-	gameView::ptr player = std::make_shared<landscapeGenView>(game);
+
+	landscapeGenView::ptr player = std::make_shared<landscapeGenView>(game);
+	player->landscape.setPosition(game, glm::vec3(1));
 	player->cam->setFar(1000.0);
 	game->setView(player);
+	setNode("enemies", game->state->rootnode, player->getEnemies());
+	setNode("landscape", game->state->rootnode, player->landscape.getNode());
 	addCameraWeapon(player);
 
 	game->run();
