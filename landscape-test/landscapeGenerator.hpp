c38dfe4681433acfee48302eed0f39a9813f8ea2
My Name <foo@lisp.us.to>
2020-12-31 03:50:03 -0500

    Spawning new entities from landscape generator events

--- landscape-test/landscapeGenerator.hpp
+++ landscape-test/landscapeGenerator.hpp
@@ -9,14 +9,35 @@
 using namespace grendx;
 using namespace grendx::ecs;
 
+struct generatorEvent {
+	// event interface for ECS stuff, this way entities can be
+	// spawned/despawned/deactivated/etc as the player moves around
+	enum types {
+		generatorStarted,
+		generated,
+		deleted,
+	} type;
+
+	glm::vec3 position;
+	glm::vec3 extent;
+};
+
+// TODO: maybe mutex, although events probably won't be pushed from workers here
+typedef std::shared_ptr<std::vector<generatorEvent>> generatorEventQueue;
+
 class worldGenerator {
 	public:
 		virtual gameObject::ptr getNode(void) { return root; };
 		virtual void setPosition(gameMain *game, glm::vec3 position) = 0;
+		virtual void setEventQueue(generatorEventQueue q);
 
 	protected:
+		// TODO: generic event class
+		virtual void emit(generatorEvent ev);
+
 		gameObject::ptr root = std::make_shared<gameObject>();
 		glm::vec3 lastPosition = glm::vec3(INT_MAX);
+		generatorEventQueue eventQueue = nullptr;
 };
 
 class landscapeGenerator : public worldGenerator {
