57b0e1af78fe757a70ec017cc451eaeba3cc17df
My Name <foo@lisp.us.to>
2021-01-24 07:13:59 -0500

    Refactoring input pads, adding rawEventHandler, touch components

--- inputHandler.hpp
+++ inputHandler.hpp
@@ -41,6 +41,20 @@ class inputHandler : public component {
 		}
 };
 
+class rawEventHandler : public component {
+	public:
+		rawEventHandler(entityManager *manager, entity *ent)
+			: component(manager, ent)
+		{
+			manager->registerComponent(ent, "rawEventHandler", this);
+		}
+
+		virtual void
+		handleEvent(entityManager *manager, entity *ent, SDL_Event& ev) {
+			std::cerr << "handleInput(): got here" << std::endl;
+		}
+};
+
 class inputPoller : public component {
 	public:
 		inputPoller(entityManager *manager, entity *ent)
@@ -54,10 +68,11 @@ class inputPoller : public component {
 
 class inputHandlerSystem : public entitySystem {
 	public:
-		typedef std::shared_ptr<entitySystem> ptr;
-		typedef std::weak_ptr<entitySystem>   weakptr;
+		typedef std::shared_ptr<inputHandlerSystem> ptr;
+		typedef std::weak_ptr<inputHandlerSystem>   weakptr;
 
 		virtual void update(entityManager *manager, float delta);
+		virtual void handleEvent(entityManager *manager, SDL_Event& ev);
 
 		inputQueue inputs = std::make_shared<std::vector<inputEvent>>();
 };
@@ -118,4 +133,47 @@ class mouseRotationPoller : public inputPoller {
 		virtual void update(entityManager *manager, entity *ent);
 };
 
+class touchMovementHandler : public rawEventHandler {
+	inputQueue inputs;
+	camera::ptr cam;
+
+	public:
+		glm::vec2 touchpos;
+		glm::vec2 center;
+		float radius;
+
+		touchMovementHandler(entityManager *manager, entity *ent, camera::ptr _cam,
+		                     inputQueue q, glm::vec2 _center, float _radius)
+			: rawEventHandler(manager, ent),
+			  center(_center), radius(_radius), inputs(q), cam(_cam)
+		{
+			manager->registerComponent(ent, "touchMovementHandler", this);
+		}
+		virtual ~touchMovementHandler() {};
+
+		virtual void handleEvent(entityManager *manager, entity *ent, SDL_Event& ev);
+};
+
+class touchRotationHandler : public rawEventHandler {
+	inputQueue inputs;
+	camera::ptr cam;
+
+	public:
+		glm::vec2 touchpos;
+		glm::vec2 center;
+		float radius;
+
+		touchRotationHandler(entityManager *manager, entity *ent, camera::ptr _cam,
+		                     inputQueue q, glm::vec2 _center, float _radius)
+			: rawEventHandler(manager, ent),
+			  center(_center), radius(_radius), inputs(q), cam(_cam)
+		{
+			manager->registerComponent(ent, "touchRotationHandler", this);
+		}
+		virtual ~touchRotationHandler() {};
+
+		virtual void handleEvent(entityManager *manager, entity *ent, SDL_Event& ev);
+};
+
 bindFunc inputMapper(inputQueue q, camera::ptr cam);
+bindFunc touchMapper(inputQueue q, camera::ptr cam);
