ae003041bbae8e829700a1f7b10a26f1eb23170a
My Name <foo@lisp.us.to>
2021-02-20 07:47:19 -0500

    Always use glew on windows

--- include/grend/openglIncludes.hpp
+++ include/grend/openglIncludes.hpp
@@ -1,25 +1,31 @@
 #pragma once
-
 #include <grend-config.h>
-#if defined(_WIN32) || defined(_WIN64)
-#include <windef.h>
+
+#if GLSL_VERSION == 100 || GLSL_VERSION == 300
+#define NO_FORMAT_CONVERSION
+#define NO_FLOATING_FB
 #endif
+
+#if defined(_WIN32) || defined(_WIN64)
+// always use glew on windows
+#include <windef.h>
+#include <GL/glew.h>
+
+#else /* android, unixen */
 
 // use glew for core versions, GLES headers for ES
 #if GLSL_VERSION == 100
 #define GL2_PROTOTYPES 1
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
-#define NO_FORMAT_CONVERSION
-#define NO_FLOATING_FB
 
 #elif GLSL_VERSION == 300
 #define GL3_PROTOTYPES 1
 #include <GLES3/gl3.h>
 #include <GLES3/gl3ext.h>
-#define NO_FORMAT_CONVERSION
-#define NO_FLOATING_FB
 
 #else
 #include <GL/glew.h>
 #endif
+
+#endif /* android, unixen */
