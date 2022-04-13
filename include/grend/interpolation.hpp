6df173b115fd80465053719d167df04d0d03a495
My Name <foo@lisp.us.to>
2022-04-13 17:31:07 -0400

    Adding a wrapper for interpolating values

--- include/grend/interpolation.hpp
+++ include/grend/interpolation.hpp
@@ -18,4 +18,59 @@ T linear(T target, T current, float factor, float delta) {
 
 // TODO: smoothstep, bicubic, etc
 
+// TODO: another version of this that does smoothstep
+template <typename T>
+class smoothed {
+	T target;
+	T current;
+	// average over 16 frames (normalized at 60fps)
+	float division = 16;
+
+	public:
+		smoothed() {};
+		smoothed(const T& initial, float div = 16.f)
+			: target(initial),
+			  current(initial),
+			  division(div)
+			{};
+
+		void update(float delta) {
+			current = average(target, current, division, delta);
+		}
+
+		operator const T& () const { return current; };
+
+		const T& get(void) const {
+			return current;
+		}
+
+		void set(T& value) {
+			target = value;
+		}
+
+		void reset() {
+			target = current = T();
+		}
+
+		void reset(T& value) {
+			target = current = value;
+		}
+
+		void operator = (const T& other) {
+			target = other;
+		}
+
+		void operator += (const T& other) {
+			target += other;
+		}
+
+		void operator -= (const T& other) {
+			target -= other;
+		}
+
+		const T& operator*() const {
+			return current;
+		}
+};
+
 };
