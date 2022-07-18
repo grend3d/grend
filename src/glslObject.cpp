288b02b54f94c8f30d433caae76cba6055798313
My Name <foo@lisp.us.to>
2022-07-17 21:55:52 -0400

    Collecting names from declarations

--- src/glslObject.cpp
+++ src/glslObject.cpp
@@ -19,6 +19,22 @@ glslObject::glslObject(std::string_view source) {
 	collectTokens(*this, tokens);
 }
 
+// XXX
+std::string collectStrs(container& tokens) {
+	std::string ret = "";
+
+	for (auto tok : tokens) {
+		ret += collect(tok.subtokens);
+
+		// handle base characters
+		if (tok.tag.size() == 0) {
+			ret += tok.data;
+		}
+	}
+
+	return ret;
+}
+
 void collectTranslationUnit(glslObject& obj, container& tokens);
 void collectExternDecl(glslObject& obj, container& tokens);
 void collectDecl(glslObject& obj, container& tokens);
@@ -60,31 +76,69 @@ void collectDeclInitList(glslObject& obj, container& tokens) {
 		throw std::logic_error("not an init_declarator_list");
 	}
 
-	auto sub = tokens[0].subtokens;
+	auto& sub = tokens[0].subtokens;
+
+	if (sub[0].tag != "single_declaration") {
+		throw std::logic_error("expected a single_declaration");
+	}
+
+	auto& firstdec = sub[0].subtokens;
+	auto& typespec = firstdec[0].subtokens;
+
+	SDL_Log("have type specifier: ");
+	dump_tokens_tree(typespec);
 
 	for (auto tok : sub) {
 		if (tok.tag == "single_declaration") {
 			collectSingleDeclaration(obj, tok.subtokens);
-			dump_tokens_tree(tok.subtokens);
+			//dump_tokens_tree(tok.subtokens);
 
 		// TODO: should have picked a better tag for this lols
 		} else if (tok.tag == "init_declarator_asdf") {
 			collectInitDeclContinued(obj, tok.subtokens);
-			dump_tokens_tree(tok.subtokens);
+			//dump_tokens_tree(tok.subtokens);
 		}
 	}
 }
 
-void collectSingleDeclaration(glslObject& obj, container& token) {
+void collectSingleDeclaration(glslObject& obj, container& tokens) {
 	SDL_Log("collectSingleDeclaration(): got here");
+	if (tokens.size() < 2) {
+		SDL_Log("Have declaration with no name? ignoring");
+		return;
+	}
+
+	auto& typespec   = tokens[0];
+	auto& identifier = tokens[1];
+
+	std::string name = collectStrs(identifier.subtokens);
+	SDL_Log("Have declaration with name: '%s'", name.c_str());
 }
 
-void collectInitDeclContinued(glslObject& obj, container& token) {
+void collectInitDeclContinued(glslObject& obj, container& tokens) {
 	SDL_Log("collectInitDeclContinued(): got here");
+
+	if (tokens.size() < 2) {
+		// TODO: rename
+		throw std::logic_error("init_declarator_asdf is too short!");
+	}
+
+	auto& identifier = tokens[1];
+	std::string name = collectStrs(identifier.subtokens);
+	SDL_Log("Have another declaration with name: '%s'", name.c_str());
 }
 
 void collectFuncDef(glslObject& obj, container& tokens) {
 	SDL_Log("collectFuncDef(): got here");
+
+	if (tokens.size() < 2) {
+		throw std::logic_error("function definition is too small!");
+	}
+
+	auto& prototype = tokens[0].subtokens;
+	auto& body      = tokens[1].subtokens;
+
+	dump_tokens_tree(prototype);
 }
 
 void collectTranslationUnit(glslObject& obj, container& tokens) {
